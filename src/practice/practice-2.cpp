843c2b444cecfffc21885282b2395a408c560e85
Fernando <fernando@aithn.net>
2026-08-29 12:31:12 -0600

    add practice-2 with set_header()

---
+++ src/practice/practice-2.cpp
@@ -0,0 +1,28 @@
+#include "../../external/fast-csv/csv.h"
+
+#include <iostream>
+
+int main()
+{
+    io::CSVReader<5,
+                  io::trim_chars<' ', '\t'>,
+                  io::double_quote_escape<';', '"'>,
+                  io::throw_on_overflow,
+                  io::single_line_comment<'#'>>
+            reader("src/practice/empleados.csv");
+
+    reader.set_header("id", "nombre", "edad", "ciudad", "salario");
+    int id;
+    std::string nombre;
+    int edad;
+    std::string ciudad;
+    double salario;
+
+    while (reader.read_row(id, nombre, edad, ciudad, salario))
+    {
+        std::cout << id << "\t" << nombre << "\t" << edad << "\t" << ciudad << "\t" << salario
+                  << "\n";
+    }
+
+    return 0;
+}
